cba392f6c19e564c6314262d200fb64309448b85
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-09-06 01:39:42 +0300

    variables

--- tests/index.js
+++ tests/index.js
@@ -3,6 +3,8 @@ import heapsort from "../lib/index";
 
 it("should sort", function(){
 	const MSG = "should be sorted";
+	const actual = heapsort([4, 3, 1, 7, 2, 5, 6, 9, 8]);
+	const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-	assert.deepEqual(heapsort([4, 3, 1, 7, 2, 5, 6, 9, 8]), [1, 2, 3, 4, 5, 6, 7, 8, 9], MSG);
+	assert.deepEqual(actual, expected, MSG);
 });
