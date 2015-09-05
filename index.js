ee25d75cba69b7721862523da24637823b152c0d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-09-06 01:33:47 +0300

    build script

--- index.js
+++
@@ -1,9 +0,0 @@
-export default heapsort;
-
-function ascendantComparator(a, b){
-	return a - b;
-}
-
-function heapsort(array, comparator=ascendantComparator) {
-	// TODO: implement heapsort
-}
--- tests.js
+++
@@ -1,8 +0,0 @@
-import assert from "assert";
-import heapsort from "./index";
-
-it('should sort', function(){
-	const MSG = "should be sorted";
-
-	assert.equal(heapsort([4, 3, 1, 7, 2, 5, 6, 9, 8]), [1, 2, 3, 4, 5, 6, 7, 8, 9], MSG);
-});
