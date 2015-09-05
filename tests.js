5a15ea5a024eca325f10ece7fb6a4ae9127507ec
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-09-06 01:26:45 +0300

    es6

--- tests.js
+++ tests.js
@@ -1,5 +1,8 @@
-var heapsort = require('./index');
+import assert from "assert";
+import heapsort from "./index";
 
 it('should sort', function(){
-	expect(heapsort([4, 3, 1, 7, 2, 5, 6, 9, 8])).toBe([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+	const MSG = "should be sorted";
+
+	assert.equal(heapsort([4, 3, 1, 7, 2, 5, 6, 9, 8]), [1, 2, 3, 4, 5, 6, 7, 8, 9], MSG);
 });
