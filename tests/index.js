acb510c25e295281a3a1ca66b1c8fa1a1d86a0c4
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-09-06 01:41:32 +0300

    used named function

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,7 @@
 import assert from "assert";
 import heapsort from "../lib/index";
 
-it("should sort", function(){
+it("should sort", function simpleSortTest() {
 	const MSG = "should be sorted";
 	const actual = heapsort([4, 3, 1, 7, 2, 5, 6, 9, 8]);
 	const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
