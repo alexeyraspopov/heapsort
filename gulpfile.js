509d1b9e54237f04de71db178eb3e0fea8a4fcee
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-09-06 01:27:59 +0300

    unnecessary file

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-	jasmine = require('gulp-jasmine');
-
-gulp.task('tests', function(){
-	return gulp.src('tests.js')
-		.pipe(jasmine());
-});
-
-gulp.task('default', ['tests']);
